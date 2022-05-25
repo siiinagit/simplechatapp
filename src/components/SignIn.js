f07fb7e73cb2f95ec489f05e93dbebc08074f888
siiina <siiinagamedebate@gmail.com>
2022-05-25 15:18:14 +0200

    some changes

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -9,7 +9,7 @@ const SignIn = () => {
   };
   return (
     <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center' }}>
-      <Button style={{ padding: '30px', fontSize: '20px', borderRadius: '0', fontWeight: '600' }} onClick={signInWithGoogle}>Sign In With Google</Button>
+      <Button style={{ padding: '30px', fontSize: '20px', borderRadius: '5px', fontWeight: '600' }} onClick={signInWithGoogle}>Sign In With Google</Button>
     </div>
   );
 };
